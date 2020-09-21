3526868a212b557ed2e8db02b683745f4c138614
Cyrille Bougot <cyrille.bougot2@laposte.net>
2020-09-22 00:03:03 +0200

    Add a script to toggle visual highlighter.

--- source/globalCommands.py
+++ source/globalCommands.py
@@ -3469,9 +3469,6 @@ class GlobalCommands(ScriptableObject):
 		recog = uwpOcr.UwpOcr()
 		recogUi.recognizeNavigatorObject(recog)
 
-	_tempEnableScreenCurtain = True
-	_waitingOnScreenCurtainWarningDialog: Optional[wx.Dialog] = None
-	_toggleScreenCurtainMessage: Optional[str] = None
 	@script(
 		# Translators: Input help mode message for toggle report CLDR command.
 		description=_("Toggles on and off the reporting of CLDR characters, such as emojis"),
@@ -3489,6 +3486,62 @@ class GlobalCommands(ScriptableObject):
 		characterProcessing.clearSpeechSymbols()
 		ui.message(state)
 
+	@script(
+		# Translators: Describes a command.
+		description=_("Toggles the state of the visual highlighter."),
+		category=SCRCAT_VISION
+	)
+	def script_toggleVisualHighlighter(self, gesture):
+		from visionEnhancementProviders.NVDAHighlighter import NVDAHighlighter
+		nvdaHighlighterId = NVDAHighlighter.getSettings().getId()
+		nvdaHighlighterInfo = vision.handler.getProviderInfo(nvdaHighlighterId)
+		alreadyRunning = bool(vision.handler.getProviderInstance(nvdaHighlighterInfo))
+
+		# Disable if running
+		if alreadyRunning:
+			# Translators: Reported when the visual highlighter is disabled.
+			message = _("Visual highlighter disabled")
+			try:
+				vision.handler.terminateProvider(nvdaHighlighterInfo)
+			except Exception:
+				# If the visual highlighter was enabled, we do not expect exceptions.
+				log.error("Visual highlighter termination error", exc_info=True)
+				# Translators: Reported when the visual highlighter could not be enabled.
+				message = _("Could not disable visual highlighter")
+			finally:
+				ui.message(message, speechPriority=speech.priorities.Spri.NOW)
+				return
+		else:
+			# Check if visual highlighter is available, exit early if not.
+			if not nvdaHighlighterInfo.providerClass.canStart():
+				# Translators: Reported when the visual highlighter is not available.
+				message = _("Visual highlighter not available")
+				ui.message(message, speechPriority=speech.priorities.Spri.NOW)
+				return
+
+			# Translators: Reported when the visual highlighter is enabled.
+			enableMessage = _("Visual highlighter enabled")
+			try:
+				vision.handler.initializeProvider(
+					nvdaHighlighterInfo,
+				)
+				nvdaHighlighterInfo.providerClass.enableInConfig(True)
+				providerInst = vision.handler.getProviderInstance(nvdaHighlighterInfo)
+				settings = providerInst.getSettings()
+				settings.highlightBrowseMode = True
+				settings.highlightFocus = True
+				settings.highlightNavigator = True
+				settings.saveSettings()
+			except Exception:
+				log.error("Visual Highlighter initialization error", exc_info=True)
+				# Translators: Reported when the visual highlighter could not be enabled.
+				enableMessage = _("Could not enable visual highlighter")
+			finally:
+				ui.message(enableMessage, speechPriority=speech.priorities.Spri.NOW)
+
+	_tempEnableScreenCurtain = True
+	_waitingOnScreenCurtainWarningDialog: Optional[wx.Dialog] = None
+	_toggleScreenCurtainMessage: Optional[str] = None
 	@script(
 		description=_(
 			# Translators: Describes a command.
